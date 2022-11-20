461d3be1e58277d7ec17047b1f5348f805bae11e
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2022-11-20 20:26:42 +0100

    menu pc

--- javascript.js
+++ javascript.js
@@ -6,3 +6,15 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function screenResize() {
+  var screenWidth = window.innerWidth;
+  var x = document.getElementById("menu--hidable");
+  if (screenWidth > 960){
+    x.style.display = "flex";
+    } else {
+    x.style.display = "none";
+  }
+}
+
+window.onresize = screenResize;
